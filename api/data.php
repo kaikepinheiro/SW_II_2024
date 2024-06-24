f4ebf6ae6cde3d0bd984d49828fb604a4b54d3a4
Kaike Pinheiro Simplicio <100208435+kaikepinheiro@users.noreply.github.com>
2024-06-24 08:01:03 -0300

    Add files via upload

---
+++ api/data.php
@@ -0,0 +1,46 @@
+<?php
+
+    return[
+        [
+            'nome' => 'Kaike',
+            'email' => 'kaike@gmail.com'
+        ],
+        [
+            'nome' => 'Lucas',
+            'email' => 'lucas@gmail.com'
+        ],
+        [
+            'nome' => 'Rafaela',
+            'email' => 'rafaela@gmail.com'
+        ],
+        [
+            'nome' => 'Laura',
+            'email' => 'laura@gmail.com'
+        ],
+        [
+            'nome' => 'Sarah',
+            'email' => 'sarah@gmail.com'
+        ],
+        [
+            'nome' => 'Gustavo',
+            'email' => 'gustavo@gmail.com'
+        ],
+        [
+            'nome' => 'Pedro',
+            'email' => 'pedro@gmail.com'
+        ],
+        [
+            'nome' => 'Yuri',
+            'email' => 'yuri@gmail.com'
+        ],
+        [
+            'nome' => 'Anderson',
+            'email' => 'Anderson@gmail.com'
+        ],
+        [
+            'nome' => 'Marcia',
+            'email' => 'marcia@gmail.com'
+        ]
+    ];
+
+?>
